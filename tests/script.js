f246b0a08245ca2f591ccc86325763ac561e2409
John Murzaku <jam@lissus.com>
2020-08-13 15:51:25 -0400

    removed js script

--- tests/script.js
+++
@@ -1,6 +0,0 @@
-// !preview r2d3 data=c(0.3, 0.6, 0.8, 0.95, 0.40, 0.20)
-//
-// r2d3: https://rstudio.github.io/r2d3
-//
-var map = new L.Map("map", {center: [37.8, -96.9], zoom: 4})
-    .addLayer(new L.TileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"));
